b5c28364d62e9b42edddb9f424d35a334ec83d7a
sato3100 <satoru4107@gmail.com>
2026-04-16 17:55:46 +0900

    LIFFをローカル環境で開発するためにhttps化

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import mkcert from "vite-plugin-mkcert";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), mkcert()],
   server: {
     host: true,
     port: 3000,
-    allowedHosts: true,
   },
 });
