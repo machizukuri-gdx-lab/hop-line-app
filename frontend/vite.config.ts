5977d0d44363530feb1a09544432747b28cdafee
sato3100 <satoru4107@gmail.com>
2026-04-30 01:40:58 +0900

    fix: mkcertをdev環境のみに限定

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import mkcert from "vite-plugin-mkcert";
 
-export default defineConfig({
-  plugins: [react(), mkcert()],
-  server: {
-    host: true,
-    port: 3000,
-  },
+export default defineConfig(async ({ command }) => {
+  const plugins = [react()];
+  if (command === "serve") {
+    const { default: mkcert } = await import("vite-plugin-mkcert");
+    plugins.push(mkcert());
+  }
+  return {
+    plugins,
+    server: {
+      host: true,
+      port: 3000,
+    },
+  };
 });
